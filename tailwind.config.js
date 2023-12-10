2105b0ef034c097bce71fc088ed8fbf42133352c
Mihhail <cryptoishere88@gmail.com>
2023-12-10 14:58:30 +0200

    init workspace

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+    content: {
+        relative: true,
+        files: [
+            './virtual_dom/src/**/*.rs',
+        ],
+    },
+    theme: {
+        extend: {
+            colors: {
+                'yew-blue': '#2b6cb0',
+                'yew-green': '#2f855a',
+                'yew-red': '#c53030',
+                'yew-yellow': '#b7791f',
+                'yew-gray': '#4a5568',
+            },
+        },
+    },
+    plugins: [
+    ]
+}
